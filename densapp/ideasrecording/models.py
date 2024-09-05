9907f4257bd21ab387b062552597087639ad098a
ngv09 <113272579+ngv09@users.noreply.github.com>
2024-09-05 11:33:33 -0500

    write initial models

--- densapp/ideasrecording/models.py
+++ densapp/ideasrecording/models.py
@@ -1,3 +1,45 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+class Idea(models.Model):
+
+    categories = [
+        'character',
+        'plot',
+        'setting',
+        'theme'
+    ]
+
+    fields = [
+        ('id',
+         models.AutoField(
+             auto_created = True,
+             primary_key = True,
+             serialize = False,
+             verbose_name = 'ID'
+         ))
+    ]
+
+    content = models.TextField()
+    title = models.TextField()
+    category = models.Choices(categories)
+    project_id = models.ForeignKey(Project.unique_id)
+
+class Project(models.Model):
+
+    fiction_genre = [
+        'Romance',
+        'Mistery',
+        'Sci-fi',
+        'Fantasy',
+        'Action',
+        'Drama',
+        'Detective',
+        'Horror',
+        'Coming of age',
+        'Comedy'
+    ]
+
+    unique_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.TextField()
+    genres = models.Choices(fiction_genre)
