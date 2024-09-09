c582edc145e7894fbdfde27a1d4bfb8534173271
ngv09 <113272579+ngv09@users.noreply.github.com>
2024-09-09 11:44:47 -0500

    play around with the views before deciding between APIView and concrete views

--- densapp/ideasrecording/views.py
+++ densapp/ideasrecording/views.py
@@ -1,27 +1,20 @@
 from django.http import JsonResponse
 from django.core import serializers
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework import status
+from rest_framework.generics import ListCreateAPIView
 from .models import Idea, Project
 
-@api_view(['POST'])
-def record_idea(request):
-    if request.method == 'POST':
-        serializer = serializers.serialize(data=request.data)
+class list_projects(ListCreateAPIView):
 
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
 
-def list_projects(request):
-    projects = Project.objects.all()
-    data = [serializers.serialize('json', projects)]
-    return JsonResponse(data, safe=False)
 
-def list_ideas_in_project(request, project_id):
-    ideas = Idea.objects.filter(project_id=project_id)
-    data = [serializers.serialize('json', ideas)]
-    return JsonResponse(data, safe=False)
+# def list_projects(request):
+#     projects = Project.objects.all()
+#     data = [serializers.serialize('json', projects)]
+#     return JsonResponse(data, safe=False)
+
+# def list_ideas_in_project(request, project_id):
+#     ideas = Idea.objects.filter(project_id=project_id)
+#     data = [serializers.serialize('json', ideas)]
+#     return JsonResponse(data, safe=False)
