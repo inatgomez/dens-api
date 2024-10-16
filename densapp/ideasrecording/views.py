661aa3114837a211919632620814ce58835c471b
ngv09 <113272579+ngv09@users.noreply.github.com>
2024-10-16 12:00:30 -0500

    write response message inside view to test connection with the front-end

--- densapp/ideasrecording/views.py
+++ densapp/ideasrecording/views.py
@@ -2,6 +2,7 @@ from rest_framework import mixins
 from rest_framework.generics import GenericAPIView
 from .serializers import IdeaSerializer, ProjectSerializer
 from .models import Idea, Project
+from rest_framework.response import Response
 
 class CreateListIdea(mixins.ListModelMixin, mixins.CreateModelMixin, GenericAPIView):
 
@@ -49,6 +50,9 @@ class CreateListProject(mixins.ListModelMixin, mixins.CreateModelMixin, GenericA
     queryset = Project.objects.all()
 
     def get(self, request, *args, **kwargs):
+        projects = self.get_queryset()
+        if not projects.exists():
+            return Response([{"message": "You'll see your projects soon!"}])
         return self.list(request, *args, **kwargs)
     
     def post(self, request, *args, **kwargs):
