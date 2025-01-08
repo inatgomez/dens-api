26ca6170f237f9aed215d5b589a7e190fc36a78c
ngv09 <113272579+ngv09@users.noreply.github.com>
2025-01-08 10:39:51 -0500

    fix urls to route API calls from the front-end to the correct view

--- densapp/ideasrecording/urls.py
+++ densapp/ideasrecording/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('projects/', views.CreateListProject.as_view()),
     path('projects/<uuid:pk>', views.RetrieveUpdateDeleteProject.as_view()),
-    path('ideas/<uuid:project>', views.CreateListIdea.as_view()),
-    path('ideas/<uuid:idea>', views.RetrieveUpdateDeleteIdea.as_view()),
+    path('ideas/<uuid:project>/ideas', views.CreateListIdea.as_view()),
+    path('ideas/<uuid:unique_id>', views.RetrieveUpdateDeleteIdea.as_view()),
 ]
 
