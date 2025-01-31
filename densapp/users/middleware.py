e7865318fc25d34d50662b1856223ec1f4926e06
ngv09 <113272579+ngv09@users.noreply.github.com>
2025-01-31 18:29:48 -0500

    return code to original state, fixes were not working

--- densapp/users/middleware.py
+++
@@ -1,8 +0,0 @@
-from django.utils.deprecation import MiddlewareMixin
-
-class OAuthStateMiddleware(MiddlewareMixin):
-    def process_request(self, request):
-        if '/o/google-oauth2/' in request.path:
-            state = request.GET.get('state')
-            if state:
-                request.session['oauth_state'] = state
--- densapp/users/views.py
+++ densapp/users/views.py
@@ -1,4 +1,3 @@
-import logging
 from django.conf import settings
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -10,29 +9,13 @@ from rest_framework_simplejwt.views import (
     TokenVerifyView
 )
 
-logger = logging.getLogger(__name__)
-
 class CustomProviderAuthView(ProviderAuthView):
     def post(self, request, *args, **kwargs):
-        stored_state = request.session.get('oauth_state')
-        incoming_state = request.data.get('state')
-        
-        if not stored_state or stored_state != incoming_state:
-            return Response(
-                {"detail": "Invalid state parameter"}, 
-                status=status.HTTP_400_BAD_REQUEST
-            )
         response = super().post(request, *args, **kwargs)
 
         if response.status_code == 200:
-            if 'oauth_state' in request.session:
-                del request.session['oauth_state']
-                
             access_token = response.data.get('access')
             refresh_token = response.data.get('refresh')
-
-            logger.debug(f"Access Token: {access_token}")
-            logger.debug(f"Refresh Token: {refresh_token}")
 
             response.set_cookie(
                 'access',
@@ -63,13 +46,9 @@ class CustomTokenObtainPairView(TokenObtainPairView):
             access_token = response.data.get('access')
             refresh_token = response.data.get('refresh')
 
-            logger.debug(f"Access Token: {access_token}")
-            logger.debug(f"Refresh Token: {refresh_token}")
-
             response.set_cookie(
                 'access',
                 access_token,
-                domain=settings.AUTH_COOKIE_DOMAIN,
                 max_age=settings.AUTH_COOKIE_ACCESS_MAX_AGE,
                 path=settings.AUTH_COOKIE_PATH,
                 secure=settings.AUTH_COOKIE_SECURE,
@@ -79,7 +58,6 @@ class CustomTokenObtainPairView(TokenObtainPairView):
             response.set_cookie(
                 'refresh',
                 refresh_token,
-                domain=settings.AUTH_COOKIE_DOMAIN,
                 max_age=settings.AUTH_COOKIE_REFRESH_MAX_AGE,
                 path=settings.AUTH_COOKIE_PATH,
                 secure=settings.AUTH_COOKIE_SECURE,
@@ -92,8 +70,6 @@ class CustomTokenObtainPairView(TokenObtainPairView):
 class CustomTokenRefreshView(TokenRefreshView):
     def post(self, request, *args, **kwargs):
         refresh_token = request.COOKIES.get('refresh')
-
-        logger.debug(f"Refresh Token from Cookie: {refresh_token}")
 
         if refresh_token:
             request.data['refresh'] = refresh_token
@@ -119,8 +95,6 @@ class CustomTokenVerifyView(TokenVerifyView):
     def post(self, request, *args, **kwargs):
         access_token = request.COOKIES.get('access')
 
-        logger.debug(f"Access Token from Cookie: {access_token}")
-
         if not access_token:
             return Response(
                 {'detail': 'Missing access token in cookies'},
@@ -135,7 +109,5 @@ class Logoutview(APIView):
         response = Response(status=status.HTTP_204_NO_CONTENT)
         response.delete_cookie('access')
         response.delete_cookie('refresh')
-
-        logger.debug("Cookies deleted on logout")
 
         return response
