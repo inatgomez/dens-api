ffc10f38a191bcf93b31c40846370eb5613121c5
ngv09 <113272579+ngv09@users.noreply.github.com>
2025-02-04 09:35:24 -0500

    update url for consistency

--- densapp/densapp/urls.py
+++ densapp/densapp/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('ideasrecording/', include('ideasrecording.urls')),
+    path('api/notes/', include('ideasrecording.urls')),
     path('api/', include('djoser.urls')),
     path('api/', include('users.urls')),
 ]
