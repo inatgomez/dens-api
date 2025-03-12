a1420a3d10863ae9a1fe80cc54c8aeda43bdc01c
ngv09 <113272579+ngv09@users.noreply.github.com>
2025-03-12 12:24:42 -0500

    use whitenoise for static files

--- densapp/build.sh
+++ densapp/build.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
-# Exit on error
 set -o errexit
-
-# Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
-
-# Apply any outstanding database migrations
+python manage.py collectstatic --no-input
 python manage.py migrate
